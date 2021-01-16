a8fb6543df4419caa7907119d34215900a88ad19
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-16 13:41:51 +0300

    Add Pydantic models

---
+++ schemas.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemUpdate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+    full_name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    is_superuser: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
