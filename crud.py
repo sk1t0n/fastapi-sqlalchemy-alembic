e3c55389dd6dbc444a3135b2ba9067fb9a0dc2b5
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-16 16:51:33 +0300

    Refactoring CRUD

--- crud.py
+++ crud.py
@@ -1,60 +1,73 @@
-from typing import List, Optional
+from typing import List, Optional, Generic, TypeVar, Type
 
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+from database import Base
 import models
 import schemas
 
-
-def create_user(db: Session, user: schemas.UserCreate) -> models.User:
-    db_user = models.User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 
-def read_users(db: Session, skip: int, limit: int) -> List[models.User]:
-    return db.query(models.User).offset(skip).limit(limit).all()
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
+        """
+        CRUD object with default methods to Create, Read, Update, Delete (CRUD).
+        **Parameters**
+        * `model`: A SQLAlchemy model class
+        * `schema`: A Pydantic model (schema) class
+        """
+        self.model = model
+
+    def create(self, db: Session, obj: CreateSchemaType) -> ModelType:
+        db_obj = self.model(**obj.dict())
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def read(self, db: Session, id: int) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def read_all(self, db: Session, skip: int, limit: int) -> List[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
+
+    def update(
+        self, db: Session, db_obj: ModelType, obj: UpdateSchemaType
+    ) -> ModelType:
+        data_to_update = obj.dict(skip_defaults=True)
+        for attr in data_to_update:
+            if hasattr(db_obj, attr):
+                setattr(db_obj, attr, data_to_update[attr])
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def delete(self, db: Session, db_obj: ModelType) -> None:
+        result = db.delete(db_obj)
+        db.commit()
+        return result
 
 
-def read_user(db: Session, id: int) -> Optional[models.User]:
-    return db.query(models.User).filter(models.User.id == id).first()
+class CRUDUser(CRUDBase[models.User, schemas.UserCreate, schemas.UserUpdate]):
+    def read_by_email(self, db: Session, email: str) -> Optional[models.User]:
+        return db.query(models.User).filter(models.User.email == email).first()
 
 
-def read_user_by_email(db: Session, email: str) -> Optional[models.User]:
-    return db.query(models.User).filter(models.User.email == email).first()
+class CRUDItem(CRUDBase[models.Item, schemas.ItemCreate, schemas.ItemUpdate]):
+    def create_item_for_user(
+        self, db: Session, user_id: int, item: schemas.ItemCreate
+    ) -> models.Item:
+        db_item = models.Item(**item.dict(), owner_id=user_id)
+        db.add(db_item)
+        db.commit()
+        db.refresh(db_item)
+        return db_item
 
 
-def update_user(
-    db: Session, user: models.User, user_schema: schemas.UserUpdate
-) -> models.User:
-    data_to_update = user_schema.dict(skip_defaults=True)
-    for attr in data_to_update:
-        if hasattr(user, attr):
-            setattr(user, attr, data_to_update[attr])
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
-
-
-def delete_user(db: Session, user: models.User) -> None:
-    result = db.delete(user)
-    db.commit()
-    print(result)
-    return result
-
-
-def read_items(db: Session, skip: int, limit: int) -> List[models.Item]:
-    return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-def create_user_item(
-    db: Session, user_id: int, item: schemas.ItemCreate
-) -> models.Item:
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
+crud_user = CRUDUser(models.User)
+crud_item = CRUDItem(models.Item)
--- endpoints.py
+++ endpoints.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from config import USERS_PER_PAGE, ITEMS_PER_PAGE
-import crud
+from crud import crud_user, crud_item
 import models
 import schemas
 
@@ -14,21 +14,21 @@ router = APIRouter()
 
 @router.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.read_user_by_email(db, user.email)
+    db_user = crud_user.read_by_email(db, user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db, user)
+    return crud_user.create(db, user)
 
 
 @router.get("/users", response_model=List[schemas.User])
 def read_users(
     skip: int = 0, limit: int = USERS_PER_PAGE, db: Session = Depends(get_db)
 ):
-    return crud.read_users(db, skip, limit)
+    return crud_user.read_all(db, skip, limit)
 
 
 def find_user(db: Session, user_id: int) -> models.User:
-    db_user = crud.read_user(db, user_id)
+    db_user = crud_user.read(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -45,25 +45,25 @@ def update_user(
     db: Session = Depends(get_db)
 ):
     db_user = find_user(db, user_id)
-    return crud.update_user(db, db_user, user_schema)
+    return crud_user.update(db, db_user, user_schema)
 
 
 @router.delete("/users/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     db_user = find_user(db, user_id)
-    return crud.delete_user(db, db_user)
+    return crud_user.delete(db, db_user)
 
 
-@router.get("/users/items", response_model=List[schemas.Item])
+@router.get("/items", response_model=List[schemas.Item])
 def read_items(
     skip: int = 0, limit: int = ITEMS_PER_PAGE, db: Session = Depends(get_db)
 ):
-    return crud.read_items(db, skip, limit)
+    return crud_item.read_all(db, skip, limit)
 
 
 @router.post("/users/{user_id}/items")
-def create_user_item(
+def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     find_user(db, user_id)
-    return crud.create_user_item(db, user_id, item)
+    return crud_item.create_item_for_user(db, user_id, item)
