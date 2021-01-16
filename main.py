c8b75dba9afca73d2294f2ca18f499e0bb845bb5
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-16 13:43:05 +0300

    Add router

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 
+from endpoints import router
+
+import models
+from database import engine
+
+# Create database tables
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello'}
-
+app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', port=8000, debug=True)
+    uvicorn.run("main:app", port=8000, debug=True)
