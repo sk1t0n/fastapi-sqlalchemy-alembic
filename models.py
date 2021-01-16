66974f9912904fff5485a6a330e388270cbcd5fb
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-16 13:41:36 +0300

    Add SQLAlchemy models

---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean(), default=False)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
