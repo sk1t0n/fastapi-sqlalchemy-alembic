0ee730fc795cd53c451622bd8473b2a649023c7d
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-17 11:35:12 +0300

    Add FastAPI metadata

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ DB_URL = f"sqlite:///{DB_NAME}.db"
 
 USERS_PER_PAGE = 5
 ITEMS_PER_PAGE = 5
+
+PROJECT_NAME = "FastAPI SQLAlchemy Alembic"
+PROJECT_DESCRIPTION = "An example of FastAPI working with SQLAlchemy and Alembic"
+PROJECT_VERSION = "0.1.0"
+API_V1_STR = "/api/v1"
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI
 import uvicorn
 
+import config
 from endpoints import router
 
 # Create database tables: alembic upgrade head
 
-app = FastAPI()
-app.include_router(router)
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title=config.PROJECT_NAME,
+    description=config.PROJECT_DESCRIPTION,
+    version=config.PROJECT_VERSION,
+    openapi_tags=tags_metadata,
+    openapi_url=f"{config.API_V1_STR}/openapi.json"
+)
+app.include_router(router, prefix=config.API_V1_STR)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, debug=True)
