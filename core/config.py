039c57702e4354e2719840da975f403b852fdfcd
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-22 15:05:46 +0300

    Change config

--- core/config.py
+++ core/config.py
@@ -8,3 +8,9 @@ PROJECT_NAME = "FastAPI SQLAlchemy Alembic"
 PROJECT_DESCRIPTION = "An example of FastAPI working with SQLAlchemy and Alembic"
 PROJECT_VERSION = "0.1.0"
 API_V1_STR = "/api/v1"
+
+# JWT, хранить все секретные данные в переменных среды или .env-файле
+# для генерации ключа ввести: openssl rand -hex 32
+SECRET_KEY = "a446bbe560387c016dad039ccd88a267c9cae0bf886c59387a416f1c1a397c05"
+ALGORITHM = "HS256"  # симметричное шифрование, один ключ на бэкенде и фронтенде
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # время действия токена в минутах
