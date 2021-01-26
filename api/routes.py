53cb3eb1d033f37c2ce78a2534a45fa9d34e9aae
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-01-26 11:27:57 +0300

    Change routes

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .endpoints import users, items
+from .endpoints import users, items, login
 
 router = APIRouter()
 router.include_router(users.router, prefix="/users", tags=["users"])
 router.include_router(items.router, prefix="/items", tags=["items"])
+router.include_router(login.router, tags=["login"])
